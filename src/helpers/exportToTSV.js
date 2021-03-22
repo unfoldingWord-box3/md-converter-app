74877e9cb56431865df4cef16bfa04bb05f50c9d
mannycolon <colonmanuel7@gmail.com>
2021-03-22 10:04:05 -0400

    Removed console logs

--- src/helpers/exportToTSV.js
+++ src/helpers/exportToTSV.js
@@ -11,8 +11,6 @@ function cleanNote(note) {
 export default function exportToTSV(sourceNotes, targetNotes, bookId, resourceId) {
   const linedUpNotes = [];
 
-  console.log({ sourceNotes, targetNotes })
-
   for (let i = 0; i < sourceNotes.length; i++) {
     const sourceNote = sourceNotes[i];
     const targetNote = targetNotes[i];
@@ -21,9 +19,6 @@ export default function exportToTSV(sourceNotes, targetNotes, bookId, resourceId
     if (targetNote?.Reference && targetNote?.Question && targetNote?.Response) {
       finalNote.Question = targetNote?.Question.trim()
       finalNote.Response = targetNote?.Response.trim()
-      console.log('targetNote?.Response.trim', targetNote?.Response.trim() + '___')
-      console.log('targetNote?.Response.split', targetNote?.Response.trim().split("\u21b5").join('') + '___')
-      console.log('targetNote?.Response.replace', targetNote?.Response.trim().replace(/\u21b5/g,'') + '___')
     } else if (targetNote?.Reference && targetNote?.Note && targetNote?.Quote) {
       finalNote.Note = cleanNote(targetNote?.Note)
     } else if (targetNote?.Reference && targetNote?.Annotation) {
@@ -33,16 +28,10 @@ export default function exportToTSV(sourceNotes, targetNotes, bookId, resourceId
       finalNote.OccurrenceNote = cleanNote(targetNote?.OccurrenceNote)
     }
 
-
-
-
     linedUpNotes.push(finalNote);
   }
 
-  console.log({ linedUpNotes })
-
   const tsvFile = parser.TSV.stringify(linedUpNotes);
-  console.log({tsvFile})
   const filename = `${resourceId?.toLowerCase() || 'tn'}_${bookId?.toUpperCase()}.tsv`;
   const blob = new Blob([tsvFile], {type: "text/plain;charset=utf-8"});
 
